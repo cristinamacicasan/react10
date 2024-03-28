3e43db7aef1ea3a5fa1db7329782927a58952b23
cristinamacicasan <cmacicasan@gmail.com>
2024-03-28 13:32:40 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import Profile from './profile'; // Importați componenta Profile
+import user from './user'; // Importați datele utilizatorului
+
 export const App = () => {
   return (
     <div
@@ -10,8 +14,15 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
 
+export default App;
